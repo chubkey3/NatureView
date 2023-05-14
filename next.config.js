4645152c6ab4bfbce9dca4a31de4053d472333e9
Jason Louie <jason.jw.louie@gmail.com>
2023-05-13 19:59:27 -0700

    pwa futher configured & env variables set

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  env: {
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    ACCESS_SECRET_KEY: process.env.ACCESS_SECRET_KEY
+  }
 }
 
-module.exports = nextConfig
+const PWAConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true
+})
+
+module.exports = PWAConfig(nextConfig);
